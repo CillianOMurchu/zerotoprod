b87fc502220c55c649ebda0065e7dfabfd600ddc
Cillian <cillian@seetransparent.com>
2023-05-01 21:08:20 +0200

    basic countdown functional

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,24 @@ fn main() {
 
     loop {
         let elapsed_time = start_time.elapsed();
-        let remaining_time = duration.checked_sub(elapsed_time).unwrap_or_else(|| Duration::from_secs(0));
+        let remaining_time = duration
+            .checked_sub(elapsed_time)
+            .unwrap_or_else(|| Duration::from_secs(0));
 
         if remaining_time.as_secs() == 0 {
             println!("Time's up!");
             break;
         }
 
-        println!("{} seconds remaining...", remaining_time.as_secs());
+        println!("{:?} seconds remaining...", format_duration(remaining_time));
 
         thread::sleep(Duration::from_secs(1));
     }
+}
+
+fn format_duration(duration: Duration) -> String {
+    let seconds = duration.as_secs();
+    let minutes = (seconds / 60) % 60;
+    let hours = seconds / 3600;
+    format!("{:02}:{:02}:{:02}", hours, minutes, seconds % 60)
 }
